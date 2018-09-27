4c61be397aefccb6c5b41e6e67281db2a614f826
aalexander3 <aralx73@gmail.com>
2018-09-26 20:13:32 -0400

    updating controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::UsersController < ApplicationController
     # deciding whether or not to use the serializer for data loading
     users = UserSerializer.new(@users).serialized_json
     render json: users
+    # should we be rendering json????
   end
 
   def show
