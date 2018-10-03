1bbc1fdbb938f567098674bc3efa8c97aa38ca32
aalexander3 <aralx73@gmail.com>
2018-10-03 09:06:08 -0400

    quick add rest of controllers

--- app/controllers/api/v1/packs_controller.rb
+++ app/controllers/api/v1/packs_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::PacksController < ApplicationController
+
+  def index
+    render json: Pack.all
+  end
+
+  def show
+    render json: @pack
+  end
+
+  def create
+    pack = Pack.new(pack_params)
+    if pack.save
+      render json: pack
+    else
+      render json: { message: "Sorry, could not create pack", errors: pack.errors.full_messages }
+    end
+  end
+
+  private
+
+  def get_pack
+    @pack = Pack.find(params[:id])
+  end
+
+  def pack_params
+    params.require(:pack).permit(:name)
+  end
+
 end
