2d4b5b901172c7e8f375ac0634e4732894abc354
Dwire <gjdwyer8@gmail.com>
2018-10-03 22:57:20 -0400

    Make Completions reference workout_pack not just workout to allow for a single workout to apear in a pack multiple times and be completed by different people

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,5 @@
 class Workout < ApplicationRecord
   has_many :workout_packs
   has_many :packs, through: :workout_packs
-  has_many :completions
+  # has_many :completions
 end
--- app/models/workout_pack.rb
+++ app/models/workout_pack.rb
@@ -1,4 +1,5 @@
 class WorkoutPack < ApplicationRecord
   belongs_to :pack, dependent: :destroy
   belongs_to :workout, dependent: :destroy
+  belongs_to :Completion, optional: true
 end
