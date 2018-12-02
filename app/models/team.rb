27ab5bb9a0ea1eb0a3b975d5a14af258e635306b
Dwire <gjdwyer8@gmail.com>
2018-12-02 18:53:11 -0500

    add teamates to team json

--- app/models/team.rb
+++ app/models/team.rb
@@ -10,7 +10,8 @@ class Team < ApplicationRecord
       league_id: self.league_id,
       name: self.name,
       motto: self.motto,
-      image_url: self.image_url
+      image_url: self.image_url,
+      teammates: self.users
     }
   end
 end
